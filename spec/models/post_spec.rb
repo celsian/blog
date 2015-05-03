91f675ae6cb7b1fbab673261d9d5f9433a4b701b
Lucas Paolella <celsian@gmail.com>
2015-05-03 12:21:54 -0700

    Added describe block for clarity

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,18 +4,20 @@ RSpec.describe Post, type: :model do
   let(:post) { FactoryGirl.create(:post) }
   let(:posts) { Post.all }
 
-  context "with no content" do
-    it "should not save" do
-      expect(Post.new.save).to be_falsey
+  describe "post model" do
+    context "with no content" do
+      it "should not save" do
+        expect(Post.new.save).to be_falsey
+      end
     end
-  end
 
-  context "new posts" do
-    it "order by date" do
-      post1 = FactoryGirl.create(:post, content: "This is the first post.")
-      post2 = FactoryGirl.create(:post, content: "This is the second post.")
+    context "new posts" do
+      it "order by date" do
+        post1 = FactoryGirl.create(:post, content: "This is the first post. Order")
+        post2 = FactoryGirl.create(:post, content: "This is the second post. Order")
 
-      expect(posts.index(post2)).to be > posts.index(post1)
+        expect(posts.index(post2)).to be > posts.index(post1)
+      end
     end
   end
 

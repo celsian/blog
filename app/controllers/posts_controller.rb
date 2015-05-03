6a7646b8412fdbe5ecbeede16183ed017cc95d25
Lucas Paolella <celsian@gmail.com>
2015-05-03 15:17:25 -0700

    Wrote create section and defined strong params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,22 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to post_path(@post), flash: {success: "Post was created."}
+    else
+      flash[:error] = "Error: #{@post.error_messages}"
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end

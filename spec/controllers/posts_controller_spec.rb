128185169b1ecb0d1683794066e461b454175f51
Lucas Paolella <celsian@gmail.com>
2015-05-03 12:21:16 -0700

    Several tests added
    Checking for index, show and new template

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,18 +4,31 @@ RSpec.describe PostsController, type: :controller do
   let(:post) { FactoryGirl.create(:post) }
   let(:posts) { Post.all }
 
-  context "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+  describe "Get #index" do
+    context "GET index" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template("index")
+      end
     end
   end
 
-  context "GET show" do
-    it "renders the show template" do
-      post1 = FactoryGirl.create(:post, content: "This is the first post.")
-      get :show, id: post1.id
-      expect(response).to render_template("show")
+  describe "Get #show" do
+    context "GET show" do
+      it "renders the show template" do
+        post1 = FactoryGirl.create(:post, content: "This is the first post. Show")
+
+        get :show, id: post1.id
+        expect(response).to render_template("show")
+      end
+    end
+  end
+
+  describe "GET #new" do
+    context "GET new" do
+      it "renders the new template" do
+        get :new
+      end
     end
   end
 

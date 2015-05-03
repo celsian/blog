0cb890d3bbe0e12b98c4368032d78cfa3ec47b3c
Lucas Paolella <celsian@gmail.com>
2015-05-03 15:38:06 -0700

    Added trait for invalid post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
+  
   factory :post do
     title ""
     content "This is a default post created by FactoryGirl's factories/posts.rb."
+
+    trait :invalid do
+      content ""
+    end
   end
 
 end

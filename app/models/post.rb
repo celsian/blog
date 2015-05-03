050aeec93a50324eb504ab5329f477bbcd3e062c
Lucas Paolella <celsian@gmail.com>
2015-05-03 15:36:56 -0700

    Added error_messages function to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ActiveRecord::Base
   validates :content, presence: true
 
   default_scope { order("created_at") }
+
+  def error_messages
+    messages = ""
+    errors.full_messages.each do |message|
+      messages += message + "."
+    end
+    messages
+  end
 end

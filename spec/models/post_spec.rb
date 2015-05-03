d66d5b8273f529c73071072f62623e8af73b9d8c
Lucas Paolella <celsian@gmail.com>
2015-05-03 11:24:33 -0700

    Added factory girl fixtures and new posts order by date test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  let(:post) { FactoryGirl.create(:post) }
+  let(:posts) { Post.all }
 
   context "with no content" do
     it "should not save" do
@@ -8,5 +10,13 @@ RSpec.describe Post, type: :model do
     end
   end
 
+  context "new posts" do
+    it "order by date" do
+      post1 = FactoryGirl.create(:post, content: "This is the first post.")
+      post2 = FactoryGirl.create(:post, content: "This is the second post.")
+
+      expect(posts.index(post2)).to be > posts.index(post1)
+    end
+  end
 
 end

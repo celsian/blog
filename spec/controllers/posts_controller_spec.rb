4640ecd2c1a9ed4b6d80aa625a67e1caf05b48f2
Lucas Paolella <celsian@gmail.com>
2015-05-04 20:59:48 -0700

    Storing test expect for later. Irrelevant for this test.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe PostsController, type: :controller do
       it "renders the index template" do
         get :index
         expect(response).to render_template("index")
+        # expect(response).to redirect_to(post_path(Post.last))
       end
     end
   end

f4766c96f51b66c3459c43587c41b2d71f1f649c
Lucas Paolella <celsian@gmail.com>
2015-05-05 20:29:29 -0700

    show_recent posts feature test

--- spec/requests/posts_features_spec.rb
+++ spec/requests/posts_features_spec.rb
@@ -12,6 +12,20 @@ RSpec.feature "posts", :type => :feature do
     end
   end
 
+  describe "show_recent template" do
+    scenario "shows up to the five most recent posts" do
+      post1 = FactoryGirl.create(:post, content: "This is the first post's content.")
+      post2 = FactoryGirl.create(:post, content: "This is the second post's content.")
+      post3 = FactoryGirl.create(:post, content: "This is the third post's content.")
+      post4 = FactoryGirl.create(:post, content: "This is the fourth post's content.")
+      post5 = FactoryGirl.create(:post, content: "This is the fifth post's content.")
+      post6 = FactoryGirl.create(:post, content: "This is the sixth post's content.")
+      visit '/posts_show_recent'
+      expect(page).to have_content "This is the sixth post's content."
+      expect(page).to have_content "This is the second post's content."
+    end
+  end
+
   describe "new template" do
     scenario "creates a new post" do
       visit new_post_path

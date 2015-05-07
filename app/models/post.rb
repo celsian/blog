ae7a8317cffa47953b10c6ea939c233cedefdeb7
Lucas Paolella <celsian@gmail.com>
2015-05-07 14:49:37 -0700

    NextMonth and PreviousMonth added, also next and previous
    Next and Previous not functioning properly, needs to be addressed

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,32 @@ class Post < ActiveRecord::Base
     end
     messages
   end
+
+########## THESE ARE NOT FUNCTIONING AS INTENDED
+  def next
+    (Post.where("posts.id > ?", self.id).order("posts.id ASC").limit(1)).first
+  end
+
+  def previous
+    (Post.where("posts.id < ?", self.id).order("posts.id DESC").limit(1)).first
+  end
+  ########## THESE ARE NOT FUNCTIONING AS INTENDED
+
+  def self.nextMonth(startTime)
+    if Post.where(created_at: (startTime+1.month)..(startTime+2.months)).length > 0
+      return ["#{(startTime+1.month).strftime("%m%y")}",
+        "#{(startTime+1.month).strftime("%B %Y")}"]
+    else
+      return false
+    end
+  end
+
+  def self.previousMonth(startTime)
+    if Post.where(created_at: (startTime-1.month)..(startTime)).length > 0
+      return ["#{(startTime-1.month).strftime("%m%y")}",
+        "#{(startTime-1.month).strftime("%B %Y")}"]
+    else
+      return false
+    end
+  end
 end

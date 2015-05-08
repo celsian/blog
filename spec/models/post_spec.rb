00ccb6e83d2b67503b08ff12df4dfb471753141d
Lucas Paolella <celsian@gmail.com>
2015-05-08 12:49:29 -0700

    Moved FactorGirl posts to a before(:each) and wrote a .next and .previous test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Post, type: :model do
   let(:posts) { Post.all }
 
   describe "post model" do
+    before(:each) do 
+      @post1 = FactoryGirl.create(:post, content: "This is the first post. Order")
+      @post2 = FactoryGirl.create(:post, content: "This is the second post. Order")
+    end
+
     context "with no content" do
       it "should not save" do
         expect(Post.new.save).to be_falsey
@@ -13,12 +18,16 @@ RSpec.describe Post, type: :model do
 
     context "new posts" do
       it "order by newest first to oldest last" do
-        post1 = FactoryGirl.create(:post, content: "This is the first post. Order")
-        post2 = FactoryGirl.create(:post, content: "This is the second post. Order")
-
-        expect(posts.index(post2)).to be < posts.index(post1)
+        expect(posts.index(@post2)).to be < posts.index(@post1)
       end
     end
-  end
 
+    it ".next returns the next post based on created_at date" do
+      expect(@post1.next.first).to be == @post2
+    end
+
+    it ".previous returns the next post based on created_at date" do
+      expect(@post2.previous.first).to be == @post1
+    end
+  end
 end

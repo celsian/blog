bf2f4afe17c0192a4764e5438c3e55b681a466bd
Lucas Paolella <celsian@gmail.com>
2015-05-10 21:37:24 -0700

    Permitted title in addition to content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:title, :content)
   end
 
 end
